517f04f191139d9ee06e5ffeba16f641cd06dbe2
Brendan Miller <brendan.miller261@gmail.com>
2021-04-13 16:34:59 +1000

    moved cell event listener into creation function

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function createGrid(xWidth, yWidth) {
+  container.innerHTML = "";
+
   for (let i = 0; i < xWidth; i++) {
     const colDiv = document.createElement("div");
 
-    for (let j = 0; j < yWidth; j++) {    
+    for (let j = 0; j < yWidth; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
 
+      cell.addEventListener("mouseenter", () => {
+        cell.classList.add("coloured");
+      });
+
       cell.style.height = "30px";
       cell.style.width = "30px";
-      
+
       colDiv.appendChild(cell);
     }
 
@@ -17,13 +23,9 @@ function createGrid(xWidth, yWidth) {
 }
 
 const container = document.getElementById("container");
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", () => {
+  createGrid(16, 16);
+});
 
 createGrid(16, 16);
-
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.classList.add("coloured");
-  });
-});
