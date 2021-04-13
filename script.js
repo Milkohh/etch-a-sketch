0d3f4c127d5eeac6d9183219d0473585dfa895ec
Brendan Miller <brendan.miller261@gmail.com>
2021-04-13 16:32:08 +1000

    Moved creation of grid into function

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+function createGrid(xWidth, yWidth) {
+  for (let i = 0; i < xWidth; i++) {
+    const colDiv = document.createElement("div");
+
+    for (let j = 0; j < yWidth; j++) {    
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+
+      cell.style.height = "30px";
+      cell.style.width = "30px";
+      
+      colDiv.appendChild(cell);
+    }
+
+    container.appendChild(colDiv);
+  }
+}
+
 const container = document.getElementById("container");
 
-// Create a grid of 16x16
-for (let i = 0; i < 16; i++) {
-  const colDiv = document.createElement("div");
-
-  for (let j = 0; j < 16; j++) {    
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-
-    cell.style.height = "30px";
-    cell.style.width = "30px";
-    
-    colDiv.appendChild(cell);
-  }
-
-  container.appendChild(colDiv);
-}
+createGrid(16, 16);
 
 const cells = document.querySelectorAll(".cell");
 
